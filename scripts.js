8f610011577bff29e96e549b57975523728e10b4
Megan <megan.mackey24@gmail.com>
2023-08-22 02:02:02 -0500

    Add on win and on lose functions
    Having them separate makes the gameRound function easier to read.
    They do probably need more functionality added to them, but are a
    good starting point for now. Maybe add function for ties as well?

--- scripts.js
+++ scripts.js
@@ -9,10 +9,23 @@ function gameRound(playerHand, cpuHand) {
     if (playerHand === cpuHand) {
         return 'tie'
     } else if ((playerHand === 'rock' || playerHand === 'paper') && (cpuHand === 'rock' || cpuHand === 'paper') ){
-        return playerHand === 'paper' ? 'win' : 'lose'
+        // return playerHand === 'paper' ? 'win' : 'lose'
+        return playerHand === 'paper' ? onWin(playerHand, cpuHand) : onLose(playerHand, cpuHand);
     } else if ((playerHand === 'paper' || playerHand === 'scissors') && (cpuHand === 'paper' || cpuHand === 'scissors') ){
-        return playerHand === 'scissors' ? 'win' : 'lose'
+        // return playerHand === 'scissors' ? 'win' : 'lose'
+        return playerHand === 'scissors' ? onWin(playerHand, cpuHand) : onLose(playerHand, cpuHand);
     } else if ((playerHand === 'scissors' || playerHand === 'rock') && (cpuHand === 'scissors' || cpuHand === 'rock') ){
-        return playerHand === 'rock' ? 'win' : 'lose'
+        // return playerHand === 'rock' ? 'win' : 'lose'
+        return playerHand === 'rock' ? onWin(playerHand, cpuHand) : onLose(playerHand, cpuHand);
     }  
+}
+
+function onWin(playerHand, cpuHand) {
+    console.log(`You played ${playerHand}. CPU played ${cpuHand}. You win.`);
+    return 'win'    
+}
+
+function onLose(playerHand, cpuHand) {
+    console.log(`You played ${playerHand}. CPU played ${cpuHand}. You lose`);
+    return 'lose'
 }
